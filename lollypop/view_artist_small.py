dd3b0a7416ab155de732bb9f93c03bb675f48914
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2019-07-18 20:14:08 +0200

    Fix args

--- lollypop/view_artist_small.py
+++ lollypop/view_artist_small.py
@@ -78,8 +78,10 @@ class ArtistViewSmall(View, ArtistViewCommon):
             position = self._scrolled.get_vadjustment().get_value()
         else:
             position = 0
+        view_type = self._view_type & ~self.view_sizing_mask
         return ({"genre_ids": self._genre_ids,
-                 "artist_ids": self._artist_ids},
+                 "artist_ids": self._artist_ids,
+                 "view_type": view_type},
                 self._sidebar_id, position)
 
     @property
