61a9b1d597a6c9b72c4e3440fcaacf25a718d135
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2019-07-22 14:39:16 +0200

    Add missing ViewType.SCROLLED

--- lollypop/view_information.py
+++ lollypop/view_information.py
@@ -97,7 +97,8 @@ class InformationView(View):
                                     ArtBehaviour.CROP_SQUARE |
                                     ArtBehaviour.CACHE,
                                     self.__on_artist_artwork)
-            albums_view = AlbumsListView([], [], ViewType.POPOVER)
+            albums_view = AlbumsListView([], [],
+                                         ViewType.POPOVER | ViewType.SCROLLED)
             albums_view.set_size_request(300, -1)
             albums_view.show()
             albums_view.set_margin_start(5)
