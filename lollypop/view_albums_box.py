1652beb97f5684d05d0cf440a85a05e9688a60c0
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2019-07-23 13:24:07 +0200

    Sould work with child album

--- lollypop/view_albums_box.py
+++ lollypop/view_albums_box.py
@@ -161,13 +161,7 @@ class AlbumsBoxView(FlowBoxView, ViewController):
         child = self._box.get_child_at_pos(x, y)
         if child is None or child.artwork is None:
             return
-        if child.is_selected() or self._view_type & ViewType.SMALL:
-            if self._genre_ids and self._genre_ids[0] == Type.YEARS:
-                album = Album(child.album.id)
-            else:
-                album = Album(child.album.id,
-                              self._genre_ids, self._artist_ids)
-            App().window.container.show_view([Type.ALBUM], album)
+        App().window.container.show_view([Type.ALBUM], child.album)
 
     def _on_secondary_press_gesture(self, x, y, event):
         """
