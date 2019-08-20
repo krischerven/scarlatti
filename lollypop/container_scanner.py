dd278831466447928280ac22cf63f0be73c0c7ee
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2019-08-20 07:16:30 +0200

    Update right list on artist-updated

--- lollypop/container_scanner.py
+++ lollypop/container_scanner.py
@@ -14,7 +14,7 @@ from gi.repository import GLib
 
 from gettext import gettext as _
 
-from lollypop.define import App, SelectionListMask
+from lollypop.define import App, SelectionListMask, Type
 
 
 class ScannerContainer:
@@ -71,12 +71,23 @@ class ScannerContainer:
             @param artist_id as int
             @param add as bool
         """
+        if Type.GENRES_LIST in self.sidebar.selected_ids:
+            selection_list = self.right_list
+            genre_ids = self.left_list.selected_ids
+        elif Type.ARTISTS_LIST in self.sidebar.selected_ids and\
+                self.left_list.mask & SelectionListMask.ARTISTS:
+            selection_list = self.left_list
+            genre_ids = []
+        else:
+            return
+        artist_ids = App().artists.get_ids(genre_ids)
+        # We only test add, remove and absent is safe
+        if artist_id not in artist_ids and add:
+            return
         artist_name = App().artists.get_name(artist_id)
         sortname = App().artists.get_sortname(artist_id)
         genre_ids = []
-        if self.left_list.get_visible() and\
-                self.left_list.mask & SelectionListMask.ARTISTS:
-            if add:
-                self.left_list.add_value((artist_id, artist_name, sortname))
-            elif not App().albums.get_ids([artist_id], genre_ids):
-                self.left_list.remove_value(artist_id)
+        if add:
+            selection_list.add_value((artist_id, artist_name, sortname))
+        elif not App().albums.get_ids([artist_id], genre_ids):
+            selection_list.remove_value(artist_id)
