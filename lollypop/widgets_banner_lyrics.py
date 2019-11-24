9391001a5da8ec53a841bd6a2c0ca1be47825b9f
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2019-11-24 13:09:54 +0100

    Fix crash in lyrics view. #2130

--- lollypop/widgets_banner_lyrics.py
+++ lollypop/widgets_banner_lyrics.py
@@ -99,7 +99,7 @@ class LyricsBannerWidget(BannerWidget, SignalsHelper):
         """
         if BannerWidget._handle_width_allocate(self, allocation):
             self.__set_artwork()
-            self.__update_font_size()
+            self.__set_internal_size()
 
     def _on_album_artwork_changed(self, art, album_id):
         """
@@ -159,7 +159,7 @@ class LyricsBannerWidget(BannerWidget, SignalsHelper):
                     ArtBehaviour.DARKER,
                     self._on_artwork)
 
-    def __update_font_size(self):
+    def __set_internal_size(self):
         """
             Set text height
         """
