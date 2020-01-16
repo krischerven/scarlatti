ec1463f84b90119f77f5edad653fd6b95a738b0d
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2020-01-16 07:06:34 +0100

    Revert "Readd audioconverters trying to fix #2171"
    This reverts commit 3d0a02c43edff85992de8daa1a31ec0c0fd45efb.

--- lollypop/player_plugins.py
+++ lollypop/player_plugins.py
@@ -38,32 +38,23 @@ class PluginsPlayer:
         try:
             bin = Gst.ElementFactory.make("bin", "bin")
 
-            ac1 = Gst.ElementFactory.make("audioconvert", "ac1")
-            bin.add(ac1)
             # Internal volume manager
             self.volume = Gst.ElementFactory.make("volume", "volume")
             self.volume.props.volume = 0.0
             bin.add(self.volume)
-            ac1.link(self.volume)
             previous_bin = self.volume
 
             # Equalizer
             if App().settings.get_value("equalizer-enabled"):
-                ac2 = Gst.ElementFactory.make("audioconvert", "ac2")
-                bin.add(ac2)
-                previous_bin.link(ac2)
                 self.__equalizer = Gst.ElementFactory.make("equalizer-10bands",
                                                            "equalizer-10bands")
                 bin.add(self.__equalizer)
-                ac2.link(self.__equalizer)
+                previous_bin.link(self.__equalizer)
                 previous_bin = self.__equalizer
 
             # Replay gain
             replay_gain = App().settings.get_enum("replay-gain")
             if replay_gain != ReplayGain.NONE:
-                ac3 = Gst.ElementFactory.make("audioconvert", "ac3")
-                bin.add(ac3)
-                previous_bin.link(ac3)
                 rgvolume = Gst.ElementFactory.make("rgvolume", "rgvolume")
                 if replay_gain == ReplayGain.ALBUM:
                     rgvolume.props.album_mode = 1
@@ -72,27 +63,24 @@ class PluginsPlayer:
                 rgvolume.props.pre_amp = App().settings.get_value(
                     "replaygain").get_double()
                 bin.add(rgvolume)
-                ac3.link(rgvolume)
+                previous_bin.link(rgvolume)
                 rglimiter = Gst.ElementFactory.make("rglimiter", "rglimiter")
                 bin.add(rglimiter)
                 rgvolume.link(rglimiter)
                 previous_bin = rglimiter
 
-            ac4 = Gst.ElementFactory.make("audioconvert", "ac4")
-            bin.add(ac4)
-            previous_bin.link(ac4)
             audiosink = Gst.ElementFactory.make("autoaudiosink",
                                                 "autoaudiosink")
             bin.add(audiosink)
-            ac4.link(audiosink)
+            previous_bin.link(audiosink)
 
             bin.add_pad(Gst.GhostPad.new(
                 "sink",
-                ac1.get_static_pad("sink")))
+                self.volume.get_static_pad("sink")))
             self.__playbin.set_property("audio-sink", bin)
             self.update_equalizer()
         except Exception as e:
-            Logger.error("PluginsPlayer::init(): %s", e)
+            Logger.error("PluginsPlayer::init():", e)
 
     def update_equalizer(self):
         """
