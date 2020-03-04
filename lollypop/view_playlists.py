e970532995ba13031446e0e48027d6b4b5cd61c5
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2020-03-04 10:07:03 +0100

    Fix a crash when showing loved tracks

--- lollypop/view_playlists.py
+++ lollypop/view_playlists.py
@@ -76,7 +76,8 @@ class PlaylistsView(FilteringHelper, LazyLoadingView, ViewController,
         def load():
             track_ids = []
             if self._playlist_id == Type.LOVED:
-                for track_id in App().tracks.get_loved_track_ids():
+                for track_id in App().tracks.get_loved_track_ids(
+                        self.storage_type):
                     if track_id not in track_ids:
                         track_ids.append(track_id)
             else:
