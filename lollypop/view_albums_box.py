fb09103ba0679ca6cff037f94711ec05ce24e2d3
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2020-03-05 09:10:41 +0100

    Remove default value

--- lollypop/view_albums_box.py
+++ lollypop/view_albums_box.py
@@ -33,8 +33,7 @@ class AlbumsBoxView(FlowBoxView, ViewController, SignalsHelper):
     """
 
     @signals_map
-    def __init__(self, genre_ids, artist_ids, storage_type,
-                 view_type=ViewType.DEFAULT):
+    def __init__(self, genre_ids, artist_ids, storage_type, view_type):
         """
             Init album view
             @param genre_ids as [int]
