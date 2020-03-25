96bf28e288f7da881cec16f6da92e2859114cb26
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2020-03-25 22:20:50 +0100

    Move this as debug message. Fix #2261

--- lollypop/lastfm.py
+++ lollypop/lastfm.py
@@ -344,7 +344,7 @@ class LastFMBase:
              @param callback as function
         """
         if attributes is None:
-            Logger.error("LastFM::__on_get_password(): no attributes")
+            Logger.debug("LastFM::__on_get_password(): no attributes")
             return
         self.__login = attributes["login"]
         self.__password = password
