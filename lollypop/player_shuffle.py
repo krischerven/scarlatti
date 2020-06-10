77ca9f95623c11f856a06e28a539e8616edaa6cb
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2020-06-10 13:52:53 +0200

    Show loading on party mode

--- lollypop/player_shuffle.py
+++ lollypop/player_shuffle.py
@@ -117,6 +117,8 @@ class ShufflePlayer:
         album_ids = App().albums.get_ids(party_ids, [], storage_type, False)
         emit_signal(self, "playback-setted", [])
         self._albums = []
+        if album_ids:
+            emit_signal(self, "loading-changed", True, Track())
         for album_id in album_ids:
             album = Album(album_id, [], [], False)
             self._albums.append(album)
