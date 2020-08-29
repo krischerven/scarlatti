a4238e55f30ec609d3fd72c4e278f7e8b0e6d079
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2020-08-29 16:11:23 +0200

    Always select first item

--- lollypop/container_lists.py
+++ lollypop/container_lists.py
@@ -300,8 +300,7 @@ class ListsContainer:
         else:
             startup_id = App().settings.get_value("startup-id").get_int32()
             if startup_id == -1:
-                if not App().window.folded:
-                    selection_list.select_first()
+                selection_list.select_first()
             else:
                 selection_list.select_ids([startup_id], True)
 
