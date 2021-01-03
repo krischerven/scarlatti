96be81e0b2a9e7722a0f75d4366320f292827c01
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2021-01-03 22:38:04 +0100

    Update Copyright

--- lollypop/menu_artwork.py
+++ lollypop/menu_artwork.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2014-2020 Cedric Bellegarde <cedric.bellegarde@adishatz.org>
+# Copyright (c) 2014-2021 Cedric Bellegarde <cedric.bellegarde@adishatz.org>
 # Copyright (c)      2020 CodedOre
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
