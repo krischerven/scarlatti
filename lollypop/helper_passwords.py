51cc612682a158274ec2ce5abdd5831442596a55
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2021-01-05 15:29:49 +0100

    Port PasswordsHelper to simple API

--- lollypop/helper_passwords.py
+++ lollypop/helper_passwords.py
@@ -12,7 +12,7 @@
 
 import gi
 gi.require_version('Secret', '1')
-from gi.repository import Secret, GLib
+from gi.repository import Secret
 
 from lollypop.logger import Logger
 
@@ -26,9 +26,7 @@ class PasswordsHelper:
         """
             Init helper
         """
-        self.__secret = None
-        Secret.Service.get(Secret.ServiceFlags.LOAD_COLLECTIONS, None,
-                           self.__on_get_secret)
+        pass
 
     def get_token(self, service):
         """
@@ -36,7 +34,6 @@ class PasswordsHelper:
             @param service as str
         """
         try:
-            secret = Secret.Service.get_sync(Secret.ServiceFlags.NONE)
             SecretSchema = {
                 "service": Secret.SchemaAttributeType.STRING
             }
@@ -46,15 +43,9 @@ class PasswordsHelper:
             schema = Secret.Schema.new("org.gnome.Lollypop",
                                        Secret.SchemaFlags.NONE,
                                        SecretSchema)
-            items = secret.search_sync(schema, SecretAttributes,
-                                       Secret.SearchFlags.UNLOCK |
-                                       Secret.SearchFlags.LOAD_SECRETS,
-                                       None)
-            if items:
-                items[0].load_secret_sync(None)
-                value = items[0].get_secret()
-                if value is not None:
-                    return value.get_text()
+            password = Secret.password_lookup_sync(schema, SecretAttributes,
+                                                   None)
+            return password
         except Exception as e:
             Logger.error("PasswordsHelper::get_token(): %s" % e)
         return None
@@ -67,7 +58,6 @@ class PasswordsHelper:
             @param args
         """
         try:
-            self.__wait_for_secret(self.get, service, callback, *args)
             SecretSchema = {
                 "service": Secret.SchemaAttributeType.STRING
             }
@@ -77,14 +67,15 @@ class PasswordsHelper:
             schema = Secret.Schema.new("org.gnome.Lollypop",
                                        Secret.SchemaFlags.NONE,
                                        SecretSchema)
-            self.__secret.search(schema, SecretAttributes,
-                                 Secret.SearchFlags.UNLOCK |
-                                 Secret.SearchFlags.LOAD_SECRETS,
-                                 None,
-                                 self.__on_secret_search,
-                                 service,
-                                 callback,
-                                 *args)
+            Secret.password_search(schema,
+                                   SecretAttributes,
+                                   Secret.SearchFlags.UNLOCK |
+                                   Secret.SearchFlags.LOAD_SECRETS,
+                                   None,
+                                   self.__on_secret_search,
+                                   service,
+                                   callback,
+                                   *args)
         except Exception as e:
             Logger.warning("PasswordsHelper::get(): %s" % e)
 
@@ -97,12 +88,6 @@ class PasswordsHelper:
             @param callback as function
         """
         try:
-            self.__wait_for_secret(self.store,
-                                   service,
-                                   login,
-                                   password,
-                                   callback,
-                                   *args)
             schema_string = "org.gnome.Lollypop: %s@%s" % (service, login)
             SecretSchema = {
                 "service": Secret.SchemaAttributeType.STRING,
@@ -132,7 +117,6 @@ class PasswordsHelper:
             @param callback as function
         """
         try:
-            self.__wait_for_secret(self.clear, service, callback, *args)
             SecretSchema = {
                 "service": Secret.SchemaAttributeType.STRING
             }
@@ -142,45 +126,28 @@ class PasswordsHelper:
             schema = Secret.Schema.new("org.gnome.Lollypop",
                                        Secret.SchemaFlags.NONE,
                                        SecretSchema)
-            self.__secret.search(schema,
-                                 SecretAttributes,
-                                 Secret.SearchFlags.UNLOCK |
-                                 Secret.SearchFlags.LOAD_SECRETS,
-                                 None,
-                                 self.__on_clear_search,
-                                 callback,
-                                 *args)
+            Secret.password_clear(schema,
+                                  SecretAttributes,
+                                  None,
+                                  self.__on_clear_search,
+                                  callback,
+                                  *args)
         except Exception as e:
             Logger.warning("PasswordsHelper::clear(): %s" % e)
 
 #######################
 # PRIVATE             #
 #######################
-    def __wait_for_secret(self, call, *args):
-        """
-            Wait for secret
-            @param call as function to call
-            @param args
-            @raise exception if waiting
-        """
-        # Wait for secret
-        if self.__secret is None:
-            GLib.timeout_add(250, call, *args)
-            raise Exception("Waiting Secret service")
-        elif self.__secret == -1:
-            raise Exception("Error waiting for Secret service")
-
     def __on_clear_search(self, source, result, callback=None, *args):
         """
             Clear passwords
             @param source as GObject.Object
             @param result as Gio.AsyncResult
+            @param callback as function
         """
         try:
             if result is not None:
-                items = source.search_finish(result)
-                for item in items:
-                    item.delete(None, None)
+                Secret.password_clear_finish(result)
             if callback is not None:
                 callback(*args)
         except Exception as e:
@@ -197,7 +164,7 @@ class PasswordsHelper:
         """
         try:
             if result is not None:
-                items = source.search_finish(result)
+                items = Secret.password_search_finish(result)
                 for item in items:
                     attributes = item.get_attributes()
                     secret = item.get_secret()
@@ -212,15 +179,3 @@ class PasswordsHelper:
         except Exception as e:
             Logger.error("PasswordsHelper::__on_secret_search(): %s" % e)
             callback(None, None, service, *args)
-
-    def __on_get_secret(self, source, result):
-        """
-            Store secret proxy
-            @param source as GObject.Object
-            @param result as Gio.AsyncResult
-        """
-        try:
-            self.__secret = source.get_finish(result)
-        except Exception as e:
-            self.__secret = -1
-            Logger.error("PasswordsHelper::__on_get_secret(): %s" % e)
