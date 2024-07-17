b6831fc4b0c42291563ad2f9a1320ed280bbbeab
Kris Cherven <krischerven@gmail.com>
2024-07-17 19:06:14 -0400

    Add warning to github-ssh-init.sh script

--- github-ssh-init.sh
+++ github-ssh-init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "Running ./github-ssh-init.sh. This file must be sourced to have any effect!"
 eval "$(ssh-agent -s)"
 # github-key should be a symlink to your github key
 ssh-add ./github-key
