090ff8d4aee692baf187a0277f529846ad210efa
Kris Cherven <krischerven@gmail.com>
2024-08-20 00:18:00 -0400

    install-flatpak.sh: Remove empty line

--- install-flatpak.sh
+++ install-flatpak.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 curl -L -o /tmp/scarlatti.flatpak https://github.com/krischerven/scarlatti/raw/master/scarlatti.flatpak
 flatpak install /tmp/scarlatti.flatpak
