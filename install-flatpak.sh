e16c0573f74132f995d76a5ad514c076214c949e
Kris Cherven <krischerven@gmail.com>
2024-09-05 16:34:16 -0400

    install-flatpak.sh: Automatically remove any existing Scarlatti installation

--- install-flatpak.sh
+++ install-flatpak.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+if [ $(flatpak list | grep -c "org.scarlatti.Scarlatti") == 1 ]; then
+   echo "Removing existing Scarlatti installation..."
+   flatpak remove org.scarlatti.Scarlatti
+fi
+
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 curl -L -o /tmp/scarlatti.flatpak https://github.com/krischerven/scarlatti/raw/master/scarlatti.flatpak
 flatpak install /tmp/scarlatti.flatpak
