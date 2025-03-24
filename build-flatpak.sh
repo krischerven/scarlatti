16a4a62a01d2ad39ef0e5a3c0464bd816e2f04eb
Kris Cherven <krischerven@gmail.com>
2025-03-23 23:43:23 -0400

    build-flatpak: Warn about unpushed commits
    This prevents building an out-of-date scarlatti.flatpak

--- build-flatpak.sh
+++ build-flatpak.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+
+
+if [ -n "$(git log --branches --not --remotes)" ]; then
+    read -p "WARNING: You have unpushed commits! Continue? (y/N) " choice
+    [[ $choice =~ ^[Yy]$ ]] || exit 1
+fi
+
 flatpak-builder --force-clean "$PWD"/flatpak "$PWD"/org.scarlatti.Scarlatti.json
 sudo flatpak build-export repo flatpak
 flatpak build-bundle repo scarlatti.flatpak org.scarlatti.Scarlatti
